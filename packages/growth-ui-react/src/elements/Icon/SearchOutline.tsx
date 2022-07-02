84d5eb8e2280445005a54396bc71c0b0803334b3
kyuhak yuk <kyuhakyuk@kyuhaks-MacBook-Pro-2.local>
2022-07-02 13:55:11 -0700

    feat: add icons

---
+++ packages/growth-ui-react/src/elements/Icon/SearchOutline.tsx
@@ -0,0 +1,22 @@
+import React, { SVGProps } from 'react';
+
+export default function SearchOutline(props: SVGProps<SVGSVGElement>): JSX.Element {
+  const color = props.color ? `var(--color-${props.color})` : 'var(--icon-color)';
+
+  return (
+    <svg
+      viewBox="0 0 20 20"
+      height="25"
+      width="25"
+      focusable="false"
+      role="img"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+      color={color}
+    >
+      <title>Search icon</title>
+      <path d="M12.9 14.32a8 8 0 111.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 108 2a6 6 0 000 12z"></path>
+    </svg>
+  );
+}
